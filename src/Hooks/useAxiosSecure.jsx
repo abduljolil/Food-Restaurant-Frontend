f752a94d42d80eaed9ba87be400978b76cd53ac4
jalil56545 <jalil.mariya.31@gmail.com>
2024-03-30 12:02:59 +0600

    added payment sction

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -24,7 +24,7 @@ const useAxiosSecure = () => {
 
     // intercepts 401 and 403 status
     axiosSecure.interceptors.response.use(function (response) {
-        console.log(response);
+        // console.log(response);
         return response;
     }, async (error) => {
         const status = error.response.status;
